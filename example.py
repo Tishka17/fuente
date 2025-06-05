d80d3edd169b4b517334ac798b00afe326898f95
Andrey Tikhonov <17@itishka.org>
2025-06-06 01:06:50 +0200

    show default value, remove default providers

--- example.py
+++ example.py
@@ -3,8 +3,8 @@ from dataclasses import dataclass
 from adaptix import P
 
 from fuente import parse
-from fuente.merger.simple import UseFirst, Unite, UseLast
-from fuente.merger_provider import MergeProvider, FixedMergeProvider, merge
+from fuente.merger.simple import UseFirst, Unite
+from fuente.merger_provider import merge
 from fuente.sources.env import EnvSource
 from fuente.sources.yaml import YamlSource
 
@@ -19,18 +19,17 @@ class Config:
     log_level: str
     database: DbConfig
     blacklist: set[str]
+    default_param: str = "default"
 
 
 cfg = parse(
-    EnvSource("MYAPP"),
+    EnvSource(prefix="MYAPP"),
     YamlSource("config.yaml"),
     YamlSource("config2.yaml"),
     recipe=[
         merge(P[Config].log_level, UseFirst()),
         merge(P[DbConfig].uri, UseFirst()),
         merge(P[Config].blacklist, Unite()),
-        MergeProvider(),
-        FixedMergeProvider(UseLast()),
     ],
     type=Config
 )
