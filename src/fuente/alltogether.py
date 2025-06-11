ec20be2c0c1615504380de77820fc80028c04b3c
Andrey Tikhonov <17@itishka.org>
2025-06-12 01:07:55 +0200

    Process special values after merging

--- src/fuente/alltogether.py
+++ src/fuente/alltogether.py
@@ -1,6 +1,6 @@
 import collections
 
-from adaptix import P, Retort, as_is_loader, bound
+from adaptix import Chain, P, Retort, as_is_loader, bound, loader
 from adaptix._internal.morphing.constant_length_tuple_provider import (
     ConstantLengthTupleProvider,
 )
@@ -12,7 +12,23 @@ from adaptix._internal.morphing.model.request_filtering import AnyModelLSC
 from adaptix._internal.provider.loc_stack_filtering import VarTupleLSC
 
 from fuente.error_mode import ErrorMode
+from fuente.merger.base import Special
 from fuente.sources.merge_source import MergeSource
+
+
+def clean_not_parsed_fields(data):
+    return {
+        k: v
+        for k, v in data.items()
+        if v is not Special.NOT_LOADED
+    }
+
+
+def check_not_parsed_fields(data):
+    if any(v is Special.NOT_LOADED for k, v in data.items()):
+        raise ValueError
+    return data
+
 
 reload_recipe = [
     bound(list, IterableProvider(dump_as=list)),
@@ -32,5 +48,11 @@ def parse(*sources, recipe, type, error_mode=ErrorMode.FAIL_ALWAYS):
     source = MergeSource(sources=sources, recipe=recipe)
     config = source.load(type, error_mode)
 
-    retort = Retort(recipe=recipe + reload_recipe, strict_coercion=False)
+    if error_mode is ErrorMode.FAIL_NOT_PARSED:
+        err_recipe = [loader(AnyModelLSC(), check_not_parsed_fields, Chain.FIRST)]
+    elif error_mode is ErrorMode.SKIP_FIELD:
+        err_recipe = [loader(AnyModelLSC(), clean_not_parsed_fields, Chain.FIRST)]
+    else:
+        err_recipe = []
+    retort = Retort(recipe=recipe + err_recipe + reload_recipe, strict_coercion=False)
     return retort.load(config, type)
--- src/fuente/error_mode.py
+++ src/fuente/error_mode.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 
 class ErrorMode(Enum):
-    SKIP_FIELD = "SKIP_FIELD"  # field skipped during parsing
+    SKIP_FIELD = "SKIP_FIELD"  # field skipped during last parsing
     SKIP_SOURCE = "SKIP_SOURCE"  # source skipped on error
-    FAIL_NOT_PARSED = "FAIL_NOT_PARSED"  # field has Special value
+    FAIL_NOT_PARSED = "FAIL_NOT_PARSED"  # field has Special value, checked during last parsing
     FAIL_ALWAYS = "FAIL_ALWAYS"  # just nothing
